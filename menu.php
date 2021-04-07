e597842e4c60b7e1c79fcc18b6818aae09a261fd
Your Name <you@example.com>
2021-04-07 18:40:41 +0700

    M

--- menu.php
+++ menu.php
@@ -24,9 +24,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <?php
-                        include_once("index.html");
-                    ?>
+                    <?php include("index.html");?>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
